ab80f5d579a8b10be935d59ed18cd42a23895bd4
Shoel4453 <165211425+Shoel4453@users.noreply.github.com>
2024-06-26 14:19:58 -0300

    Add files via upload

---
+++ Proyecto pagina/js/main.js
@@ -0,0 +1,16 @@
+const SliderIni = document.getElementById('SliderIni')
+
+SliderIni.addEventListener('slide.bs.carousel', event => {
+  
+
+
+
+})
+
+
+
+
+const carousel = new bootstrap.Carousel(SliderIni, {
+    interval: 2000,
+    touch: false
+  })
